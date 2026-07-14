e67f22c646d6a2ca6c2ee8aaf483936f35a5b76c
pvta <honda43233@gmail.com>
2026-07-14 09:06:04 +0700

    khoi tao du an

---
+++ src/main/java/com/honda/managerlmsmini/ManagerLmsMiniApplication.java
@@ -0,0 +1,13 @@
+package com.honda.managerlmsmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerLmsMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerLmsMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/honda/managerlmsmini/ManagerLmsMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honda.managerlmsmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerLmsMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
